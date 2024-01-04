be2e8fbb5a5981e3070ab99de0e5f23d0d2144a2
dhiraj-chakma <chakmadhiraj@gmail.com>
2024-01-04 23:31:16 +0100

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Main App component
+const App = () => {
+  // Sample data for stories
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  // State hook for search term
+  const [searchTerm, setSearchTerm] = React.useState("React");
 
+  // Handler for search input changes
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter stories based on search term
+  const searchedStories = stories.filter((story) =>
+    story.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  );
+
+  // Render the main App component
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch} search={searchTerm} />
+      <hr />
 
-export default App
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+// Search component for input field
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>
+);
+
+// List component to display stories
+const List = ({ list }) =>
+(
+    <ul>
+      {list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+;
+
+// Item component for displaying each story
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span>
+        {" "}
+        <a href={item.url}>{item.title}</a>
+      </span>
+
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  );
+};
+
+export default App;
